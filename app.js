fd9e718109b6c561669dce9ab2addec2f7230b85
Fazzke Fraktal <Melancholerisch@gmail.com>
2023-08-02 22:57:28 +0200

    changed greeting message again

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Fazzke!'));
+app.get('/', (req, res) => res.send('Hello Fazzke! Supertümpel!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+//https://aws.amazon.com/getting-started/hands-on/create-continuous-delivery-pipeline/module-two/?e=gs2020&p=cicd-one
